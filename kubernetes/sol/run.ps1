e7eb9c9ac6787912538cbdabc78bf44917ad7e41
chenzhe <chenzhe0722@163.com>
2021-06-01 10:33:46 +0800

    Add support for helm
    1. Add helm configuration, remove support for pure kubectl command.
    2. Update dependencies for project.
    3. Replace mysql group replication with mariadb galera cluster.

--- kubernetes/sol/run.ps1
+++
@@ -1,8 +0,0 @@
-Function Apply-Kube(${KubeDir} ${KubeConf}) {
-  foreach(${Conf} in Get-Content "${PSScriptRoot}\${KubeConf}") {
-    kubectl apply -f "${PSScriptRoot}\${KubeDir}\${Conf}"
-  }
-}
-
-Apply-Kube "main" "main.conf"
-Apply-Kube ${args}[0] "mode.conf"
